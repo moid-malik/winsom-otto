0029f906ff8f25dfc366719cac2bf6ef60fa5b98
Moid Malik <138004003+moid-malik@users.noreply.github.com>
2025-03-12 17:11:30 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/shared/Header";
 import Footer from "@/components/shared/Footer";
